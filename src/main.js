7d31da19226fc1cb2e4592c2b548538298e9d680
vyuba <preyealexander731@gmail.com>
2025-08-19 21:18:38 +0100

    logging the req

--- src/main.js
+++ src/main.js
@@ -11,17 +11,12 @@ export default async ({ req, res, log, error }) => {
   // const users = new Users(client);
 
   // try {
-  log(req.bodyText); // Raw request body, contains request data
+  log(req); // Raw request body, contains request data
   // log(JSON.stringify(req.bodyJson)); // Object from parsed JSON request body, otherwise string
   // log(JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
-  log(req.scheme); // Value of the x-forwarded-proto header, usually http or https
-  log(req.method); // Request method, such as GET, POST, PUT, DELETE, PATCH, etc.
   log(req.url); // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
   log(req.host); // Hostname from the host header, such as awesome.appwrite.io
   log(req.port); // Port from the host header, for example 8000
-  log(req.path); // Path part of URL, for example /v1/hooks
-  log(req.queryString); // Raw query params string. For example "limit=12&offset=50"
-  log(JSON.stringify(req.query));
   // pcIndex.upsert([
   //   {
   //     id: '1',
