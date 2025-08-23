3534281f8fc63f27945d2df0f000c130b05b3eea
vyuba <preyealexander731@gmail.com>
2025-08-23 15:11:13 +0100

    added shopify also want to use the funtion to store products in pinecone

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { Client, Users } from 'node-appwrite';
+import { Client, Databases } from 'node-appwrite';
+import shopify from './shopify';
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
@@ -8,36 +9,100 @@ export default async ({ req, res, log, error }) => {
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(req.headers['x-appwrite-key'] ?? '');
-  // const users = new Users(client);
 
-  // try {
-  // log(req); // Raw request body, contains request data
-  log(JSON.stringify(req.bodyJson)); // Object from parsed JSON request body, otherwise string
-  // log(JSON.stringify(req.headers)); // String key-value pairs of all request headers, keys are lowercase
-  log(req.url); // Full URL, for example: http://awesome.appwrite.io:8000/v1/hooks?limit=12&offset=50
-  log(req.host); // Hostname from the host header, such as awesome.appwrite.io
-  log(req.port); // Port from the host header, for example 8000
-  // pcIndex.upsert([
-  //   {
-  //     id: '1',
-  //     values: [1, 2, 3],
-  //     metadata: {
-  //       name: 'bunny-bite',
-  //     },
-  //   },
-  // ]);
-  // } catch (err) {
-  //   error('Could not list users: ' + err.message);
-  // }
+  const database = new Databases(client);
 
-  return res.json({
-    success: true,
-  });
+  const { shop, $id } = req.bodyJson;
+
+  //init shopify
+
+  // const { shopify, appwritesessionStorage } = await getShopify();
+
+  const session = await database.getDocument(
+    process.env.DATABASE_ID,
+    process.env.SESSION_COLLECTION_ID,
+    `offline_${shop}`
+  );
+  console.log('session:', session);
+
+  if (!session) {
+    return res.json({ error: 'Could not find a session' }, { status: 404 });
+  }
+
+  try {
+    //query for getting products
+
+    const queryString = `query {
+            products(first: 10) {
+                nodes {
+                    id
+                    title
+                    description
+                    priceRangeV2 {
+                      minVariantPrice {
+                        amount
+                        currencyCode
+                      }
+                      maxVariantPrice {
+                        amount
+                        currencyCode
+                      }
+                    }
+                }
+            }
+          }`;
+
+    // shopify client initialization
+
+    const client = new shopify.clients.Graphql({ session });
+    const products = await client.request(queryString);
+
+    // logging when getting products error
+
+    if (products.errors) {
+      console.log(products.errors);
+      return NextResponse.json({ status: 400, error: products.errors });
+    }
+
+    // creating an embedding for the products
+
+    const productsEmbedingResponse = await Promise.all(
+      products.data.products.nodes.map(async (product) => {
+        const productsEmbeding = await model.embedQuery(
+          // JSON.stringify(product)
+          `${product.title}. ${product.description}. ${product.priceRangeV2.minVariantPrice.amount}`
+        );
+        return {
+          id: product.id,
+          values: productsEmbeding,
+          metadata: {
+            title: product.title,
+            description: product.description,
+            price: product.priceRangeV2.minVariantPrice.amount,
+            shop: shop,
+            shopId: $id,
+          },
+        };
+      })
+    );
+
+    // Storing in pinecone index
+
+    await pcIndex.upsert(productsEmbedingResponse);
+
+    return res.json(
+      {
+        success: true,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    log(error);
+    return res.json(
+      {
+        success: false,
+      },
+      { status: 500 }
+    );
+  }
 };
-
-// // The req object contains the request data
-// if (req.path === "/ping") {
-//   // Use res object to respond with text(), json(), or binary()
-//   // Don't forget to return a response!
-//   return res.text("Pong");
-// }
---
+++ src/shopify.js
@@ -0,0 +1,17 @@
+import { shopifyApi, LATEST_API_VERSION } from '@shopify/shopify-api';
+
+export default shopify = shopifyApi({
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET,
+  scopes: [
+    'read_orders',
+    'write_orders',
+    'read_customers',
+    'write_customers',
+    'read_products',
+  ],
+  hostName: new URL(`https://${process.env.NEXT_PUBLIC_SHOPIFY_APP_URL}`).host,
+  apiVersion: LATEST_API_VERSION,
+  isEmbeddedApp: false,
+  useOnlineTokens: false,
+});
